203ee408502c4e5ca72271249620610984e1b70d
Tim Hitchins <timhitchins@pop-os.localdomain>
2022-05-22 22:52:13 -0700

    add simple test

--- R/terraformer.R
+++ R/terraformer.R
@@ -1,33 +1,6 @@
-
-# library(V8)
-
-# terraformer <- v8()
-# terraformer$source("https://unpkg.com/terraformer@1.0.8/terraformer.js")
-
-# terraformer$assign("vehicles", cars[1:3, ])
-# terraformer$get("vehicles")
-# cat(terraformer$eval("JSON.stringify(vehicles, null, 2);"))
-
-
 #' @export
-test <- function() {
-
-
-
-    # terraformer$call("Terraformer.ArcGIS.parse", V8::JS("{x: -122.6764, y: 45.5165, spatialReference: {wkid: 4326}}"))
-}
-
-#' @export
-#' // parse an ArcGIS Geometry to GeoJSON
 parseArcGISGeometry <- function(json) {
-    json <- jsonlite::toJSON(
-        list(
-            x = -122.6764,
-            y = 45.5165,
-            "spatialReference" = list(wkid = 4326)
-        ),
-        auto_unbox = TRUE
-    )
+    # parse an ArcGIS Geometry to GeoJSON
 
     terraformer$call(
         "Terraformer.ArcGIS.parse",
--- tests/testthat/test-parser.R
+++ tests/testthat/test-parser.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("parser returns valid geojson", {
+  json <- jsonlite::toJSON(
+    list(
+      x = -122.6764,
+      y = 45.5165,
+      "spatialReference" = list(wkid = 4326)
+    ),
+    auto_unbox = TRUE
+  )
+
+  arc_geometry <- parseArcGISGeometry(json)
+
+  expect_true(is.list(arc_geometry))
 })
